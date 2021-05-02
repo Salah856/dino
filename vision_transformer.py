42b0645b70acda0842a4d65401e7d27f29e988ee
Salah Othman <salah_elhossiny856@yahoo.com>
2021-05-02 13:07:27 +0200

    Update vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -18,7 +18,7 @@ def drop_path(x, drop_prob: float = 0., training: bool = False):
     keep_prob = 1 - drop_prob
     shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
     random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
-    random_tensor.floor_()  # binarize
+    random_tensor.floor_()  
     output = x.div(keep_prob) * random_tensor
     return output
 
